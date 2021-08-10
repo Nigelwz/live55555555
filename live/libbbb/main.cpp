b95c219e447884c4a751d0d8be7b990d5c5ec97a
Ericw <Ericw.wang@primax.com.tw>
2021-08-10 19:09:57 +0800

    init

---
+++ live/libbbb/main.cpp
@@ -0,0 +1,153 @@
+#include "liveMedia.hh"
+#include "BasicUsageEnvironment.hh"
+#include <iostream>
+
+//using namespace std;
+
+// Used to shut down and close a stream (including its "RTSPClient" object):
+void shutdownStream(RTSPClient* rtspClient, int exitCode = 1);
+
+
+// A function that outputs a string that identifies each stream (for debugging output).  Modify this if you wish:
+UsageEnvironment& operator<<(UsageEnvironment& env, const RTSPClient& rtspClient) {
+  return env << "[URL:\"" << rtspClient.url() << "\"]: ";
+}
+
+// A function that outputs a string that identifies each subsession (for debugging output).  Modify this if you wish:
+UsageEnvironment& operator<<(UsageEnvironment& env, const MediaSubsession& subsession) {
+  return env << subsession.mediumName() << "/" << subsession.codecName();
+}
+
+
+
+char eventLoopWatchVariable = 0;
+
+void usage(UsageEnvironment& env, char const* progName) {
+  	env << "Usage: " << progName << " <rtsp-url-1> ... <rtsp-url-N>\n";
+ 	env << "\t(where each <rtsp-url-i> is a \"rtsp://\" URL)\n";
+}
+
+int  main(int argc, char** argv)
+{
+	TaskScheduler* scheduler = BasicTaskScheduler::createNew();
+ 	UsageEnvironment* env = BasicUsageEnvironment::createNew(*scheduler);
+  	// We need at least one "rtsp://" URL argument:
+*env << "fadfsaf";
+ 
+
+  	if (argc < 2) {
+    	usage(*env, argv[0]);
+    	return 1;
+  	}
+*env << "fadfsaf";
+  	// There are argc-1 URLs: argv[1] through argv[argc-1].  Open and start streaming each one:
+  	for (int i = 1; i <= argc-1; ++i) {
+    //	openURL(*env, argv[0], argv[i]);
+  	}
+
+ 	// All subsequent activity takes place within the event loop:
+  	env->taskScheduler().doEventLoop(&eventLoopWatchVariable);
+	return 0;
+}
+
+
+class StreamClientState {
+public:
+  StreamClientState();
+  virtual ~StreamClientState();
+
+public:
+  MediaSubsessionIterator* iter;
+  MediaSession* session;
+  MediaSubsession* subsession;
+  TaskToken streamTimerTask;
+  double duration;
+};
+
+// If you're streaming just a single stream (i.e., just from a single URL, once), then you can define and use just a single
+// "StreamClientState" structure, as a global variable in your application.  However, because - in this demo application - we're
+// showing how to play multiple streams, concurrently, we can't do that.  Instead, we have to have a separate "StreamClientState"
+// structure for each "RTSPClient".  To do this, we subclass "RTSPClient", and add a "StreamClientState" field to the subclass:
+
+class ourRTSPClient: public RTSPClient {
+public:
+  static ourRTSPClient* createNew(UsageEnvironment& env, char const* rtspURL,
+				  int verbosityLevel = 0,
+				  char const* applicationName = NULL,
+				  portNumBits tunnelOverHTTPPortNum = 0);
+
+protected:
+  ourRTSPClient(UsageEnvironment& env, char const* rtspURL,
+		int verbosityLevel, char const* applicationName, portNumBits tunnelOverHTTPPortNum);
+    // called only by createNew();
+  virtual ~ourRTSPClient();
+
+public:
+  StreamClientState scs;
+};
+
+
+#if 0
+#define RTSP_CLIENT_VERBOSITY_LEVEL 1 // by default, print verbose output from each "RTSPClient"
+
+static unsigned rtspClientCount = 0; // Counts how many streams (i.e., "RTSPClient"s) are currently in use.
+
+void openURL(UsageEnvironment& env, char const* progName, char const* rtspURL) {
+  // Begin by creating a "RTSPClient" object.  Note that there is a separate "RTSPClient" object for each stream that we wish
+  // to receive (even if more than stream uses the same "rtsp://" URL).
+  RTSPClient* rtspClient = ourRTSPClient::createNew(env, rtspURL, RTSP_CLIENT_VERBOSITY_LEVEL, progName);
+  if (rtspClient == NULL) {
+    env << "Failed to create a RTSP client for URL \"" << rtspURL << "\": " << env.getResultMsg() << "\n";
+    return;
+  }
+
+  ++rtspClientCount;
+
+  // Next, send a RTSP "DESCRIBE" command, to get a SDP description for the stream.
+  // Note that this command - like all RTSP commands - is sent asynchronously; we do not block, waiting for a response.
+  // Instead, the following function call returns immediately, and we handle the RTSP response later, from within the event loop:
+  rtspClient->sendDescribeCommand(continueAfterDESCRIBE); 
+}
+
+
+// Implementation of the RTSP 'response handlers':
+
+void continueAfterDESCRIBE(RTSPClient* rtspClient, int resultCode, char* resultString) {
+  do {
+    UsageEnvironment& env = rtspClient->envir(); // alias
+    StreamClientState& scs = ((ourRTSPClient*)rtspClient)->scs; // alias
+
+    if (resultCode != 0) {
+      env << *rtspClient << "Failed to get a SDP description: " << resultString << "\n";
+      delete[] resultString;
+      break;
+    }
+
+    char* const sdpDescription = resultString;
+    env << *rtspClient << "Got a SDP description:\n" << sdpDescription << "\n";
+
+    // Create a media session object from this SDP description:
+    scs.session = MediaSession::createNew(env, sdpDescription);
+    delete[] sdpDescription; // because we don't need it anymore
+    if (scs.session == NULL) {
+      env << *rtspClient << "Failed to create a MediaSession object from the SDP description: " << env.getResultMsg() << "\n";
+      break;
+    } else if (!scs.session->hasSubsessions()) {
+      env << *rtspClient << "This session has no media subsessions (i.e., no \"m=\" lines)\n";
+      break;
+    }
+
+    // Then, create and set up our data source objects for the session.  We do this by iterating over the session's 'subsessions',
+    // calling "MediaSubsession::initiate()", and then sending a RTSP "SETUP" command, on each one.
+    // (Each 'subsession' will have its own data source.)
+    scs.iter = new MediaSubsessionIterator(*scs.session);
+    setupNextSubsession(rtspClient);
+    return;
+  } while (0);
+
+  // An unrecoverable error occurred with this stream.
+  shutdownStream(rtspClient);
+}
+
+
+#endif
